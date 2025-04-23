0eba23243b532248c33fa202b0c2586a72953347
raju12208160 <mailraju1406@gmail.com>
2025-04-23 22:56:41 +0530

    Full Updated....

--- AgroBoost/urls.py
+++ AgroBoost/urls.py
@@ -21,10 +21,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
 
-
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('', RedirectView.as_view(url='/accounts/login/', permanent=False)),
+    path("accounts/", include("accounts.urls")),
+    path("", RedirectView.as_view(url="/accounts/login/", permanent=False)),
+]
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve static and media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
